ff2fef0be1a3467708e69f7b27ef53f157e747bc
fschaal <f.schaal@janssen1877.com>
2016-08-15 12:20:16 +0200

    set template engine to ejs

--- app.js
+++ app.js
@@ -8,17 +8,11 @@ var port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.set('views', './src/views');
 
-var handlebars = require('express-handlebars');
-app.engine('.hbs', handlebars({
-    extname: '.hbs'
-}));
-
-app.set('view engine', '.hbs');
+app.set('view engine', 'ejs');
 
 app.get('/', function (request, response) {
     response.render('index', {
-        title: 'Hi for handle',
-        list: ['a', 'b']
+        title: 'Hi from ejs'
     });
 });
 
